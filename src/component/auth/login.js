f67341ee90c38512a4217cebb52e1b5905bdb6ca
rhinel <rhinelkc@hotmail.com>
2018-05-29 23:19:50 +0800

    完成初步结构

--- src/component/auth/login.js
+++ src/component/auth/login.js
@@ -48,55 +48,53 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="page">
-        <div className="main-wrap">
-          <Card className="login-wrap">
-            <div className="login-hello">
-              Hello { this.state.logininfo.name } <i />
-            </div>
+      <div className="login-wrap">
+        <Card className="login-card">
+          <div className="login-hello">
+            Hello { this.state.logininfo.name } <i />
+          </div>
 
-            <Form
-              ref="logininfo"
-              model={this.state.logininfo}
-              rules={this.state.loginrules}
-              onSubmit={this.onSubmit.bind(this)}>
-              <Form.Item prop="name">
-                <Input
-                  value={this.state.logininfo.name}
-                  maxLength={10}
-                  placeholder="Name"
-                  icon="star-on"
-                  onChange={this.onChange.bind(this, 'name')} />
-              </Form.Item>
-              <Form.Item prop="pwd">
-                <Input
-                  value={this.state.logininfo.pwd}
-                  type="password"
-                  placeholder="Pwd"
-                  icon="star-on"
-                  onChange={this.onChange.bind(this, 'pwd')} />
-              </Form.Item>
-              <Button
-                loading={this.state.logininfo.loading}
-                className="login-go"
-                type="primary"
-                nativeType="submit">
-                登陆
-              </Button>
-            </Form>
+          <Form
+            ref="logininfo"
+            model={this.state.logininfo}
+            rules={this.state.loginrules}
+            onSubmit={this.onSubmit.bind(this)}>
+            <Form.Item prop="name">
+              <Input
+                value={this.state.logininfo.name}
+                maxLength={10}
+                placeholder="Name"
+                icon="star-on"
+                onChange={this.onChange.bind(this, 'name')} />
+            </Form.Item>
+            <Form.Item prop="pwd">
+              <Input
+                value={this.state.logininfo.pwd}
+                type="password"
+                placeholder="Pwd"
+                icon="star-on"
+                onChange={this.onChange.bind(this, 'pwd')} />
+            </Form.Item>
+            <Button
+              loading={this.state.logininfo.loading}
+              className="login-go"
+              type="primary"
+              nativeType="submit">
+              登陆
+            </Button>
+          </Form>
 
-            <div className="beian">
-              <div>v{this.state.version}</div>
-              <a
-                href="http://www.miitbeian.gov.cn/"
-                rel="noopener noreferrer"
-                target="_blank">
-                粤ICP备17164727号
-              </a>
-            </div>
+          <div className="beian">
+            <div>v{this.state.version}</div>
+            <a
+              href="http://www.miitbeian.gov.cn/"
+              rel="noopener noreferrer"
+              target="_blank">
+              粤ICP备17164727号
+            </a>
+          </div>
 
-          </Card>
-        </div>
+        </Card>
       </div>
     );
   }
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 
 import login from './component/auth/login';
@@ -9,8 +10,25 @@ import login from './component/auth/login';
 export default () => (
   <Router>
     <div className="router-wrap">
-      <Route path="/" exact component={login} />
-      <Route path="/login" component={login} />
+      <Switch>
+        <Route path="/inner" render={() => (
+          <div className="inner-wrap">
+            <div className="inner-head-wrap">
+              头部
+            </div>
+            <div className="inner-page-wrap">
+              内部页面
+            </div>
+          </div>
+        )} />
+
+        <Route path="/" render={() => (
+          <div className="auth-wrap">
+            <Route path="/login" component={login} />
+            <Route path="/" exact component={login} />
+          </div>
+        )} />
+      </Switch>
     </div>
   </Router>
 )
