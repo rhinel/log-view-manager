1dbeb2c40a7555cb20b680f38736c88a68514cb3
rhinel <rhinelkc@hotmail.com>
2018-06-12 13:56:08 +0800

    增加服务端脚本

---
+++ logviewmanager.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# 删除原有容器
+docker rm -f logviewmanager
+
+# 启动并自动重启
+docker run \
+ --name logviewmanager \
+ --link myredis:myredis \
+ --link mymongo:mymongo \
+ --expose 80 \
+ --expose 443 \
+ -v /etc/localtime:/etc/localtime \
+ -v ~/gitcode/log-view-manager:/webapp \
+ -w /webapp/server-node \
+ -d node node index
